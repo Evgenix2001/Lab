f17f9dea0d1685bfedab74fc9f7f17d182630647
Evgenix2001 <70563042+Evgenix2001@users.noreply.github.com>
2020-09-29 00:09:58 +0300

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -1,10 +1,14 @@
-let Ready = confirm ("Are you ready?");
+function Ready() {
+let confirm = ("Are you ready?");
 alert ("Ready");
-let Years = promt ('What is your age?');
+}
+function Years() {
+let promt =  ('What is your age?');
 if (Years >=18){
 alert("Good Luck!");
 } else {
 alert("U are allowed to continue but I'm take no responsibility");
+}
 }
 function Sqrt() {
   let input = document.getElementById("input_id").value;
